71790a5754872df2c802a77a66495c9d08af5aba
asadidebuger <asadidebuger@users.noreply.github.com>
2022-12-03 19:30:54 +0000

    deploy: e69ff04150732b2ac06aeae0d8438da545f1d30a

---
+++ _nuxt/2eaba58.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{455:function(n,t,e){"use strict";e.r(t);var c=e(31),component=Object(c.a)({},(function(){var n=this;return(0,n._self._c)("div",[n._t("nav"),n._v(" "),n._t("content")],2)}),[],!1,null,null,null);t.default=component.exports}}]);
